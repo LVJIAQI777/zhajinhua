fa95ed570db0dbdfbbad1716ff2c25e15b6d6ca2
QI <lvjiaqi1998@gmail.com>
2022-07-10 22:22:16 +0800

    demo1

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main() {
     while (sign) {
         cout << "请输入玩家人数：";
         cin >> num_player;
+//        if(num_player)
         players = new Player[num_player];
 
         for (int i = 0; i < num_player; ++i) {
